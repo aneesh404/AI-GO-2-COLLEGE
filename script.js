b2af7523daa03236f74b35dbde5a6a56fb613a36
aneesh404 <aneeshchawla404@gmail.com>
2020-09-07 19:19:17 +0530

    Squashed a bad bug

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // WebSpeech Configurations
+
 const searchForm = document.querySelector("#search-form");
 const searchFormInput = searchForm.querySelector("input");
 window.addEventListener("DOMContentLoaded", () => {
@@ -112,19 +113,6 @@ function sendRequestToWit(data) {
 //   } 
 // }); 
 
-$(document).ready(function(){
-  $('#myinput').keypress(function(e){
-    if(e.keyCode==13)
-    // $('#myButt').click();
-    handleInput();
-  });
-});
-
-
-
-$("#GFG_Button").click(function() { 
-  alert("Button clicked"); 
-}); 
 
 var myPix = ["Counselors/doggo1.jpg","Counselors/doggo2.jpg", "Counselors/doggo3.jpg"];
 function choosePic(){
@@ -278,7 +266,7 @@ function handleCollegeQuery(data){
     setReply(`${name} has ${vals[9]}`);
   }
   else if(command == 'Rank'){
-    setReply(`${name} is ranked ${vals[11]} around the world.`);
+    setReply(`${name} is ranked ${vals[11]} in the global rankings.`);
   }
   else if(command == 'Location'){
     setReply(`${name} is located in ${vals[12]}`);
